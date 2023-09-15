416f4385d9238665443fe6cda6667db8d5eb6ff1
IlyaGrebenschikov <ilushagr22@gmail.com>
2023-09-15 23:43:06 +0400

    test

---
+++ ilusha_test/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import requests
+import time
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    data = {
+        'title': 'Про нас'
+    }
+    return render(request, 'main/about.html', data)
